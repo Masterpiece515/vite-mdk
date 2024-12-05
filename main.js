2426ccef3fc23b32a8cc70c235f1b36e60d54a6a
Masterpiece515 <mosnova228@gmail.com>
2024-12-05 10:35:20 +0900

    swipper

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+import Swiper from "swiper";
+import "swiper/css";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+Swiper.use([Pagination]);
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  observer: true,
+  breakpoints: {
+    1119: {
+      enabled: false,
+    }, 
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+});
+
 const burger = document.querySelector('.burger');
 const menu  = document.querySelector('.menu');
 
